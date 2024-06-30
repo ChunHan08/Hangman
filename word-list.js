e03812a3e0f7b05a074d95257a6c5a15d490d0a2
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-06-30 23:33:03 +0000

    Initial commit

---
+++ word-list.js
@@ -0,0 +1,46 @@
+const wordList = [
+  {
+  word: "symphony",
+  hint: "A long musical composition for a full orchestra, typically in multiple movements."
+  },
+  { 
+  word: "adventure",
+  hint: "An exiting or daring experience."
+  },
+  {
+  word: "telephone",
+  hint: "A device used to transmit sound over long distances."
+  },
+  {
+  word: "sunset",
+  hint: "The daily disappearance of the sun below the horizon."
+  },
+  {
+  word: "shadow",
+  hint: "A dark area or shape produced by an object blocking the light."
+  },
+  {
+  word: "chocolate",
+  hint: "A sweet treat made from cocoa beans."
+  },
+  {
+  word: "vibrant",
+  hint: "Full of energy, brightnes, and life."
+  },
+  {
+  word: "illusion",
+  hint: "A false percepction or belief; a deceptive appearance or impression."
+  },
+  {
+  word: "mysterious",
+  hint: "Difficult or impossible to understand, explain, or identify."
+  },
+  {
+  word: "whisper",
+  hint: "To speak very softly or quietly, often in a secretive manner
+  }
+  {
+    
+  }
+
+]
