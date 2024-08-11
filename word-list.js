b334335f9bbe0d8a9171294f8274e1bd865d0481
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-08-11 00:20:35 +0000

    update

--- word-list.js
+++ word-list.js
@@ -1,16 +1,45 @@
 const wordList = [
-  {
     {
-      word: "Invisible",
-      hint: "She don't even know you"
+      word: "timmcgraw",
+      hint: "I hope you think my favourite song"
     },
     {
-      word: "Fearless",
-      hint: "Run your hairs through your hair"
+      word: "theoutside",
+      hint: "Nobody ever let's mein"
     },
     {
-      word: "Fifteen",
+      word: "stay beautiful",
+      hint: "Your're beautiful, every little piece love",
+    },
+  
+    {
+      word: "maryssong",
+      hint: "Take me back when our world was one block wide"
+    },
+    
+    {
+      word: "invisible",
+      hint: "She doesn't even know you"
+    },
+    {
+      word: "fearless",
+      hint: "Run your hair through your hair"
+    },
+    {
+      word: "fifteen",
       hint: "Take a deep breath, go"
+    },
+    {
+      word: "lovestory",
+      hint: "Escape this town for a little while"
+    },
+    {
+      word: "heystephan",
+      hint: "'Cause I can't help it if you look like an angel",
+    },
+    {
+      word: "whitehorse",
+      hint: "you had to fight to have the upper hand",
     },
     {
       word: "Change",
@@ -120,4 +149,88 @@ const wordList = [
       word: "mirrorball",
       hint: "And when I break it's in a million pieces"
     },
-  ]
+    {
+      word: "peter",
+      hint: "My lost fearless leader"
+    },
+    {
+      word: "glitch",
+      hint: "Nights are so starry, blood moonlit"
+    },
+    {
+      word: "paris",
+      hint: "Some wannabe Z-lister"
+    },
+    {
+      word: "labyrinth",
+      hint: "I thought the plane was going down"
+    },
+    {
+      word: "karma",
+      hint: "My pennies made your crown"
+    },
+  {
+    word: "Question",
+    hint: "But 15 seconds later they were clapping too?"
+  },
+  {
+    word: "bejeweled",
+    hint: "Familiarity breeds contempt"
+  },
+  {
+    word: "closure",
+    hint: "Don't treat me like some situation that needs to be handled"
+  },
+  {
+    word: "maroon",
+    hint: "In New York, no shoes"
+  },
+  {
+    word: "ivy",
+    hint: "My pain fits in the palm of your freezing hand"
+  },
+  {
+    word: "marjorie",
+    hint: "What died didn't stay dead"
+  },
+  {
+    word: "happiness",
+    hint: "Honey, when I'm above the trees"
+  },
+  {
+    word: "dorothea",
+    hint: "Do you ever stop and think about me?"
+  },
+  {
+    word: "evermore",
+    hint: "Ive been down since July"
+  },
+  {
+    word: "hoax",
+    hint: "My smoking gun"
+  },
+  {
+    word: "willow",
+    hint: "And there was one prize I'd cheat to win"
+  },
+  {
+    word: "epiphany",
+    hint: "Only 20 minutes to sleep"
+  },
+  {
+    word: "betty",
+    hint: "Or lead me to the garden?"
+  },
+  {
+    word: "peace",
+    hint: "I never had the courage of my convictions"
+  },
+  {
+    word: "seven",
+    hint: "With Pennsylvania under me"
+  },
+  {
+    word: "august",
+    hint: "But I can see us lost in the memory"
+  },
+  ];
